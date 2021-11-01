e3488aabc00a25df72b1ec97b5721dc7fb7fab5f
unknown <yoshuah809@gmail.com>
2021-11-01 12:14:25 -0500

    updated urls path

--- music_project/music/urls.py
+++ music_project/music/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('music/', views.SongList.as_view()),
+    #path('127.0.0.1:8000/', views.SongList.as_view()),
+    
 
 ]
--- music_project/music_project/urls.py
+++ music_project/music_project/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('127.0.0.1:8000/music/', include('music.urls')),
+    path('', include('music.urls')),
    
 
 
